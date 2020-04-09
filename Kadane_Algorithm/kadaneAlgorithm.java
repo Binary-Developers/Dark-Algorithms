1ff8b4f1b47b4ea39e2aff360917e2059110f83e
parjanyaacoder <46294122+parjanyaacoder@users.noreply.github.com>
2020-04-09 20:12:34 +0530

    Edited kadaneAlgorithm.java

--- Kadane_Algorithm/kadaneAlgorithm.java
+++ Kadane_Algorithm/kadaneAlgorithm.java
@@ -1,15 +1,15 @@
 import java.util.*;
  class abc
-{   static long findMaxSumArray(long arr[],long dp[],int x)
+{   static long findMaxSumArray(long arr[],int x)
     {   long currentSum=0,maxSum=0;int i;
-        if(x==1)
-        return dp[0];
-        for(i=1;i<x;i++)
-        {currentSum=Math.max(dp[i-1],(dp[i-1]+arr[i]));
-        dp[i]=Math.max(currentSum,arr[i]);}
+        
         for(i=0;i<x;i++)
-         if(maxSum<=dp[i])
-             maxSum=dp[i]; 
+        {currentSum+=arr[i];
+            if(currentSum<0)
+            currentSum=0;
+        maxSum=Math.max(currentSum,maxSum);}
+        
+       
         return maxSum;
     }
     public static void main()
@@ -20,13 +20,12 @@ import java.util.*;
         long a[]=new long[n];
         for(i=0;i<n;i++)
         a[i]=sc.nextLong();
-        long dp[]=new long[n];
-        dp[0]=a[0];
-        sum=findMaxSumArray(a,dp,n);
+        
+        sum=findMaxSumArray(a,n);
         System.out.println(sum);
     }
 }
-/* Input/Output :
+/* Input/Output-
 5
 4 2 -3 -1 8
-19 */
+10 */
