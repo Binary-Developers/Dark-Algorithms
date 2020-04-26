aa1a3d84540b60a19b00e790cd6cf86b076a5e4d
parjanyaacoder <46294122+parjanyaacoder@users.noreply.github.com>
2020-04-26 11:08:50 +0530

    Update and rename tetranacciNumbers.java to tetranacciNumbers/tetranacciNumbers.java

--- tetranacciNumbers/tetranacciNumbers.java
+++ tetranacciNumbers/tetranacciNumbers.java
@@ -20,9 +20,15 @@ public class Main
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number");
         int limit=sc.nextInt();int answer;
         int dp[]=new int[limit+1];
         answer=tetranacciNumbers(limit,dp);
         System.out.println(answer);
     }
 }
+/* Input/Output - 
+ Enter number
+ 10 
+ 208 */
+ 
